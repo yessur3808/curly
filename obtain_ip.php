cbc6f70fad9468280911517998b5c35170f115c1
yessur3808 <yessur3808@users.noreply.github.com>
2017-10-16 17:11:29 +0800

    Update obtain_ip.php

--- obtain_ip.php
+++ obtain_ip.php
@@ -1,36 +1,32 @@
 <?php
 
-
-// get user details
-        $user_agent = $_SERVER['HTTP_USER_AGENT']; 					    //user browser
-        $ip_address = $_SERVER["REMOTE_ADDR"];     					    // user ip adderss
-        $page_name = $_SERVER["SCRIPT_NAME"];     					    // page the user looking
-        $query_string = $_SERVER["QUERY_STRING"];   					// what query he used
-        $current_page = $page_name."?".$query_string; 
-
-
-    // get location 
-        $url = json_decode(file_get_contents("http://api.ipinfodb.com/v3/ip-city/?key=/*userapikey*/
-        // you can get your api key form http://ipinfodb.com/
-        ip=".$_SERVER['REMOTE_ADDR']."&format=json"));
-        $country=$url->countryName;  								// user country
-        $city=$url->cityName;       								// city
-        $region=$url->regionName;  									// region
-        $latitude=$url->latitude;    								//lat and lon
-        $longitude=$url->longitude;
-
-    // get time
-        date_default_timezone_set('UTC');
-        $date = date("Y-m-d");
-        $time = date("H:i:s");
-		
+// Function to get the client IP address
+function get_client_ip() {
+    $ipaddress = '';
+    if (getenv('HTTP_CLIENT_IP'))
+        $ipaddress = getenv('HTTP_CLIENT_IP');
+    else if(getenv('HTTP_X_FORWARDED_FOR'))
+        $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
+    else if(getenv('HTTP_X_FORWARDED'))
+        $ipaddress = getenv('HTTP_X_FORWARDED');
+    else if(getenv('HTTP_FORWARDED_FOR'))
+        $ipaddress = getenv('HTTP_FORWARDED_FOR');
+    else if(getenv('HTTP_FORWARDED'))
+       $ipaddress = getenv('HTTP_FORWARDED');
+    else if(getenv('REMOTE_ADDR'))
+        $ipaddress = getenv('REMOTE_ADDR');
+    else
+        $ipaddress = 'UNKNOWN';
+    return $ipaddress;
+	echo $ipaddress;
+}
 		
 		
 		
 		
 echo "Your IP is";
-echo $ip_address;
 
+ get_client_ip();
 
 
 
